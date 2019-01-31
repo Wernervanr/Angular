89a7787145a2f64de62f5a26ed2e94f68ffb8057
Werner van Randeraat <werner.van.randeraat@youngcoders.nl>
2019-01-31 10:14:25 +0100

    Test #3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MyTestComponent } from './my-test/my-test.component';
+import { AddUserComponent } from './users/add-user/add-user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyTestComponent,
+    AddUserComponent
   ],
   imports: [
     BrowserModule
